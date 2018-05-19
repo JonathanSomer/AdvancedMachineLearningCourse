1b426ed4aec4561ebe3e33439988ae3f5df11d7d
uridabomb <uriavron@gmail.com>
2018-05-19 15:26:23 +0300

    updated instructions and adds sample and some coding

--- exercises/4/variational_autoencoder.py
+++ exercises/4/variational_autoencoder.py
@@ -17,13 +17,14 @@ from keras import backend as K
 from keras import metrics
 from keras.datasets import mnist
 
+import pandas as pd
+
 batch_size = 100
 original_dim = 784
 latent_dim = 2
 intermediate_dim = 256
 epochs = 50
 epsilon_std = 1.0
-
 
 x = Input(shape=(original_dim,))
 h = Dense(intermediate_dim, activation='relu')(x)
@@ -32,10 +33,11 @@ z_log_var = Dense(latent_dim)(h)
 
 
 def sampling(args):
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
+    _z_mean, _z_log_var = args
+    epsilon = K.random_normal(shape=(K.shape(_z_mean)[0], latent_dim), mean=0.,
                               stddev=epsilon_std)
-    return z_mean + K.exp(z_log_var / 2) * epsilon
+    return _z_mean + K.exp(_z_log_var / 2) * epsilon
+
 
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
 
@@ -57,7 +59,6 @@ vae.add_loss(vae_loss)
 vae.compile(optimizer='rmsprop')
 vae.summary()
 
-
 # train the VAE on MNIST digits
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -71,3 +72,44 @@ vae.fit(x_train,
         epochs=epochs,
         batch_size=batch_size,
         validation_data=(x_test, None))
+
+# new code starts here
+encoder = Model(x, z_mean)
+
+x_encoded_mean = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(x_encoded_mean)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(x_encoded_mean, _x_decoded_mean)
+
+# save the models
+vae.save('vae.h5')
+encoder.save('encoder.h5')
+generator.save('generator.h5')
+
+# (c)
+print('\nCorresponding mapping coordinates in the latent space - one image per digit:\n')
+encoded_x_test = encoder.predict(x_test)
+digits = {}
+i = 0
+while len(digits) < 10:
+    digits[y_test[i]] = encoded_x_test[i]
+    i += 1
+
+print(pd.DataFrame(digits))
+
+
+# (d)
+z_sample = np.array([[-2.5, 0.55]])
+decoded_x = generator.predict(z_sample)
+plt.imshow(np.reshape(decoded_x, (28, 28)), interpolation='nearest')
+plt.savefig('d.png', format='png')
+
+
+# (e)
+four, nine = digits[4], digits[9]
+(x1, y1), (x2, y2) = four, nine
+a = (y2 - y1) / (x2 - x1)
+b = y1 - (a * x1)
+f = lambda _x_: a * _x_ + b
+x_samples = np.linspace(x1, x2, num=10)
+samples = [np.array([[_x, f(_x)]]) for _x in x_samples]
