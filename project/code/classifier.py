f382673dd84124ac6dd423f04abd664f8218e97a
uridabomb <uriavron@gmail.com>
2018-08-18 19:25:29 +0300

    Formatting and adds option to set trainablity

--- project/code/classifier.py
+++ project/code/classifier.py
@@ -8,34 +8,35 @@ from keras.optimizers import Adam
 
 
 class Classifier(object):
-	def __init__(self, n_classes=15, model_weights_file_path=None):
-		
-		self.model = self.new_model(n_classes)
+    def __init__(self, n_classes=15, model_weights_file_path=None, trainable=True):
+        self.model = Classifier.new_model(n_classes, trainable=trainable)
 
-		if model_weights_file_path is not None:
-			self.model.load_weights(model_weights_file_path)
-		
-		optimizer = self.get_optimizer()		
-		self.model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        if model_weights_file_path is not None:
+            self.model.load_weights(model_weights_file_path)
 
-	def new_model(self, n_classes):
-	    a = Input(shape=(7, 7, 2048,))
-	    b = GlobalMaxPooling2D()(a)
-	    b = Dense(n_classes, activation='softmax')(b)
-	    return Model(a,b)
+        optimizer = Classifier.get_optimizer()
+        self.model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-	def get_optimizer(self):
-		return Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    @staticmethod
+    def new_model(n_classes, trainable=True):
+        a = Input(shape=(7, 7, 2048,))
+        b = GlobalMaxPooling2D(trainable=trainable)(a)
+        b = Dense(n_classes, activation='softmax', name='lclassifier', trainable=trainable)(b)
+        return Model(a, b)
 
-	# supply file_path if want to save model to file
-	def fit(self, X_train, y_train, model_weights_file_path=None):
-	    self.model.fit(X_train, y_train, batch_size=50, epochs=10, verbose=2, validation_split=0.1)
+    @staticmethod
+    def get_optimizer():
+        return Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
-	    if model_weights_file_path is not None:
-	        self.model.save(model_weights_file_path)
-	        
-	    return self.model
+    # supply file_path if want to save model to file
+    def fit(self, X_train, y_train, model_weights_file_path=None):
+        self.model.fit(X_train, y_train, batch_size=50, epochs=10, verbose=2, validation_split=0.1)
 
-	 # returns an array with [loss, accuracy]
-	def evaluate(self, X_test, y_test):
-		return self.model.evaluate(X_test, y_test, batch_size=32, verbose=1)
+        if model_weights_file_path:
+            self.model.save(model_weights_file_path)
+
+        return self.model
+
+    # returns an array with [loss, accuracy]
+    def evaluate(self, X_test, y_test):
+        return self.model.evaluate(X_test, y_test, batch_size=32, verbose=1)
