434a06528d036864016709aa33866d698f569ee9
Jonathan Somer <jonathan.somer@fiverr.com>
2018-08-19 23:20:50 +0300

    add extract_image_features

--- project/code/data_utils.py
+++ project/code/data_utils.py
@@ -45,7 +45,7 @@ def get_processed_data(num_files_to_fetch_data_from):
 
     for i in range(1, min(13, num_files_to_fetch_data_from + 1)):
         print("fetching data from file #%d" % i)
-        file_path = DATA_DIRECTORY + 'image_data_' + str(i) + '.pickle'
+        file_path = READ_ONLY_PROCESSED_DATA + 'image_data_' + str(i) + '.pickle'
         bytes_in = bytearray(0)
         input_size = os.path.getsize(file_path)
 
@@ -83,3 +83,96 @@ def remove_diseases(X, y, diseases_to_remove, data):
     black_list = le.transform(diseases_to_remove)
     include = ~np.isin(y, black_list)
     return X[include], y[include]
+
+def images_directory(i):
+    return DATA_DIRECTORY + 'images_' + str(i) + '/'
+
+def get_single_disease_images_dataframe():
+    df = pd.read_csv(DATA_DIRECTORY + 'Data_Entry_2017.csv')
+    single_disease_images = df.loc[~df['Finding Labels'].str.contains('\|')]
+    return single_disease_images
+
+def file_names_by_directory(directory):
+    return [f for f in listdir(directory) if isfile(join(directory, f))]
+
+def get_resnet_model():
+    return ResNet50(weights='imagenet', include_top=False)
+
+
+
+# in order to extract image features into pickle files run:
+# df = get_single_disease_images_dataframe()
+# model = get_resnet_model()
+# extract_image_features(model, df)
+
+# link to kaggle data: https://www.kaggle.com/nih-chest-xrays/data
+# note: must have images_1,....images_12 directories of images from kaggle in data/ directory
+#       must have Data_Entry_2017.csv from kaggle in data/ directory
+def extract_image_features(model=None, single_disease_images_dataframe=None):
+
+    if model is None:
+        model = get_resnet_model()
+
+    if single_disease_images_dataframe is None:
+        single_disease_images_dataframe = get_single_disease_images_dataframe()
+
+    le = preprocessing.LabelEncoder()
+    le.fit(single_disease_images_dataframe['Finding Labels'])
+    label_encoder_classes = le.classes_
+
+    cnt = 0
+
+    for file_index in range(1,2):
+
+        file_indexes = []
+        image_names = []
+        features = []
+        int_labels = []
+
+        directory = images_directory(file_index)
+        names_of_images_in_directory = pd.Series(file_names_by_directory(directory))
+        single_disease_images_in_directory_bool = names_of_images_in_directory.isin(single_disease_images_dataframe['Image Index'])
+        single_disease_images_in_directory = names_of_images_in_directory[single_disease_images_in_directory_bool]
+
+        for image_name in single_disease_images_in_directory:
+
+
+            image_path = directory + image_name
+            img = image.load_img(image_path, target_size=(224, 224))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            image_features = model.predict(x)
+
+            text_label = single_disease_images_dataframe.loc[single_disease_images_dataframe['Image Index'] == image_name]['Finding Labels'].values[0]
+
+            file_indexes.append(file_index)
+            image_names.append(image_name)
+            features.append(image_features)
+            int_labels.extend(le.transform([text_label]))
+
+
+            cnt += 1
+            if cnt % 10 == 0:
+                print("working on file #%d , handled %d images" % (file_index, cnt))
+
+        data = {
+        'file_indexes' : file_indexes,
+        'image_names' : image_names,
+        'features' : features,
+        'int_labels' : int_labels,
+        'label_encoder_classes' : label_encoder_classes
+        }
+
+        d = pickle.dumps(data)
+        with open(DATA_DIRECTORY + 'image_data_' + str(file_index) + '.pickle', 'wb') as f:
+            for i in range(0, len(d), MAX_BYTES):
+                f.write(d[i:i+MAX_BYTES])
+
+
+def write_large_object_to_file(obj, path):
+    d = pickle.dumps(obj)
+    max_bytes = 2**31 - 1
+    with open(path, 'wb') as f:
+        for i in range(0, len(d), max_bytes):
+            f.write(d[i:i+max_bytes])
