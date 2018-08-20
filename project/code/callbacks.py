aaa86c474848f63d9222991e39abb352d38e03fc
uridabomb <uriavron@gmail.com>
2018-08-20 21:13:26 +0300

    adjustments

--- project/code/callbacks.py
+++ project/code/callbacks.py
@@ -3,13 +3,13 @@ import requests
 
 
 class CloudCallback(Callback):
-    def __init__(self, remote=False, slack_url='', stop_url='', slack_channel='', model=''):
+    def __init__(self, remote=False, slack_url='', stop_url='', slack_channel='', name=''):
         super(CloudCallback, self).__init__()
         self.remote = remote
         self.slack_url = slack_url
         self.slack_channel = slack_channel
         self.stop_url = stop_url
-        self.model = model
+        self.name = name
 
     def stop_instance(self):
         if self.remote:
@@ -17,19 +17,20 @@ class CloudCallback(Callback):
             requests.get(self.stop_url)
 
     def send_update(self, msg):
-        print(msg.replace(':weight_lifter', '🏋').replace(':tada:', '🎉'))
+        print(msg.replace(':weight_lifter:', '🏋').replace(':tada:', '🎉'))
         if self.remote:
             payload = {'message': msg, 'channel': self.slack_channel}
             requests.post(self.slack_url, json=payload)
 
-    def on_train_begin(self, logs=None):
-        self.send_update('Training {model} has just started :weight_lifter:'.format(model=self.model))
+    # def on_train_begin(self, logs=None):
+    #     self.send_update('Training {name} has just started :weight_lifter:'.format(name=self.name))
 
     def on_epoch_end(self, epoch, logs={}):
-        loss = logs.get('pos_loss') or logs.get('loss')
-        acc = logs.get('pos_acc') or logs.get('acc')
+        # loss = logs.get('pos_loss') or logs.get('loss')
+        acc = logs.get('acc')
 
-        self.send_update('*Epoch {0} has ended*! Loss: `{1}` - Accuracy: `{2}`'.format(epoch + 1, loss, acc))
+        self.send_update('*Epoch {0} has ended*! - Accuracy: `{1}`'.format(epoch + 1, acc))
+        # self.send_update('*Epoch {0} has ended*! Loss: `{1}` - Accuracy: `{2}`'.format(epoch + 1, loss, acc))
 
-    def on_train_end(self, logs=None):
-        self.send_update('Training is done :tada:')
+    # def on_train_end(self, logs=None):
+    #     self.send_update('Training is done :tada:')
