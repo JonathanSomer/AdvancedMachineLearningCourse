0daf3fbc48fedf04d72a34eec8b19abcb35649da
uridabomb <uriavron@gmail.com>
2018-08-28 20:28:05 +0300

    adds cross validation function

--- project/code/collect.py
+++ project/code/collect.py
@@ -29,8 +29,12 @@ dataset_to_lowshot_func = {'xray': to_low_shot_xray,
 
 def update(msg):
     print(msg)
-    # payload = {'message': msg, 'channel': config.slack_channel}
-    # requests.post(config.slack_url, json=payload)
+
+
+def slack_update(msg):
+    print(msg)
+    payload = {'message': msg, 'channel': config.slack_channel}
+    requests.post(config.slack_url, json=payload)
 
 
 def cosine_similarity(a, b):
@@ -63,14 +67,16 @@ def preprocess(dataset_name, n_files, verbose=False):
     return cat_to_vectors, original_shape
 
 
-def process_centroids(dataset_name, n_files, n_clusters, cat_to_vectors, n_jobs, verbose=False):
+def process_centroids(dataset_name, n_files, n_clusters, cat_to_vectors, n_jobs, verbose=False, evaluate=False):
     if dataset_name == 'xray':
         centroids_name = 'centroids_nf.{0}_nc.{1}'.format(n_files, n_clusters)
     else:
         centroids_name = '{0}_centroids_{1}_clusters'.format(dataset_name, n_clusters)
 
+    cat_to_inertia = {}
+
     read_path = du.read_pickle_path(centroids_name)
-    if os.path.exists(read_path):
+    if os.path.exists(read_path) and not evaluate:
         if verbose:
             update('Loaded centroids from file')
         cat_to_centroids = joblib.load(read_path)
@@ -81,10 +87,15 @@ def process_centroids(dataset_name, n_files, n_clusters, cat_to_vectors, n_jobs,
             update('Running KMeans to get {0} clusters/centroids for `{1}`.'.format(n_clusters, category))
             kmeans = KMeans(n_clusters=n_clusters, n_jobs=n_jobs, verbose=0).fit(X)
             cat_to_centroids[category] = kmeans.cluster_centers_
+            cat_to_inertia[category] = kmeans.inertia_
 
         write_path = du.write_pickle_path(centroids_name)
         joblib.dump(cat_to_centroids, write_path)
         update('done to cluster :tada: centroids saved as *{0}*'.format(centroids_name))
+
+    if evaluate:
+        avg_inertia = sum(cat_to_inertia.values()) / len(cat_to_inertia)
+        return avg_inertia
 
     return cat_to_centroids
 
@@ -150,6 +161,34 @@ def load_quadruplets(n_clusters, categories='all', n_files=12, dataset_name='xra
     return quadruplets, cat_to_centroids, cat_to_vectors, original_shape
 
 
+def cross_validate(dataset_name='mnist', min_n=10, max_n=40, init_step=10, n_jobs=8, n_files=12):
+    cat_to_vectors, original_shape = preprocess(dataset_name, n_files)
+
+    best = max_n
+    low, high, step = min_n, max_n, init_step
+    while step > 0:
+        inertias = {low: process_centroids(dataset_name, n_files, low, cat_to_vectors, n_jobs, evaluate=True),
+                    high: process_centroids(dataset_name, n_files, low, cat_to_vectors, n_jobs, evaluate=True)}
+        for n in range(low + step, high, step):
+            inertias[n] = process_centroids(dataset_name, n_files, n, cat_to_vectors, n_jobs, evaluate=True)
+
+        best = min(inertias, key=lambda n: inertias[n])
+        slack_update('(*[n_clusters cross validation]* current best is _{0}_'.format(best))
+        if best in (low, high):
+            return best
+
+        if inertias[low] < inertias[high]:
+            high = best
+        elif inertias[low] > inertias[high]:
+            low = best
+        else:
+            slack_update('WEIRD (best == low == high) = {0}'.format(low))
+
+        step //= 2
+
+    return best
+
+
 def main(dataset_name, n_files, n_clusters, n_jobs, test):
     if test:
         n_files = 1
@@ -171,12 +210,19 @@ def main(dataset_name, n_files, n_clusters, n_jobs, test):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dataset', help='what dataset to use', default='xray')
+    parser.add_argument('-d', '--dataset', help='what dataset to use', default='mnist')  # xray is the second one
     parser.add_argument('-f', '--n_files', help='number of files to process', type=int, default=12)
     parser.add_argument('-c', '--n_clusters', help='number of clusters to create', type=int, default=20)
     parser.add_argument('-j', '--n_jobs', help='number of jobs to do in parallel', type=int, default=8)
     parser.add_argument('-t', '--test', help='is it a test run or not', action='store_true')
+    parser.add_argument('-cv', '--cross_validate', help='whether to run cross validation', action='store_true')
+    parser.add_argument('--min_n', help='min number of clusters to cross validate on', type=int, default=10)
+    parser.add_argument('--max_n', help='max number of clusters to cross validate on', type=int, default=40)
+    parser.add_argument('--init_step', help='init step to use for cross validation', type=int, default=10)
 
     args = parser.parse_args()
 
-    main(args.dataset, args.n_files, args.n_clusters, args.n_jobs, args.test)
+    if args.cross_validate:
+        cross_validate(args.dataset, args.min_n, args.max_n, args.init_step, args.n_jobs, args.n_files)
+    else:
+        main(args.dataset, args.n_files, args.n_clusters, args.n_jobs, args.test)
