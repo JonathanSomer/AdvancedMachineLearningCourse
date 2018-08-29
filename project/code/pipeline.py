bcfc4484332e0b1006d8508abd935f088490097f
= <matan.harel.mh@gmail.com>
2018-08-29 17:00:14 +0300

    move data object function from mnist_data to data_object.py

--- project/code/pipeline.py
+++ project/code/pipeline.py
@@ -27,7 +27,7 @@ class PipeLine:
     def evaluate_cls(self, removed_inx=None):
         results = {}
 
-        y_score = self.cls.model.predict(self.dataset.x_test)
+        y_score = self.cls.model.predict(self.dataset.into_evaluate()[0])
         fpr = dict()
         tpr = dict()
         n_classes = self.dataset.get_num_classes()
@@ -68,6 +68,64 @@ class PipeLine:
 
         fig.savefig('%s.png' % figure_name.replace(" ", "_"), dpi=fig.dpi)
 
+    def get_low_shot_results(self, inx):
+        low_shot_learning_results = {}
+        X, y = get_features_and_labels(raw_data)
+        le = get_label_encoder(raw_data)
+
+        for disease in queries_diseases:
+            disease_path = os.path.join('low_shot', disease)
+            _logger.info('\t %s' % disease)
+            low_shot_learning_results[disease] = {}
+
+            novel_disease_label = le.transform((disease,))[0]
+            X_train, X_test, y_train, y_test = new_get_train_test_split_without_disease(X, y, disease, raw_data)
+            disease_base_cls = Classifier(n_classes=N_CLASSES - 1, n_epochs=1)
+            disease_base_cls.fit(X_train, new_onehot_encode(y_train, [novel_disease_label]))
+            cls_results, fpr, tpr = evaluate_cls(disease_base_cls, X_test, y_test,
+                                                 diseases_removed=[novel_disease_label])
+            create_cls_roc_plot(fpr, tpr, cls_results, '%s base cls' % disease, disease_path)
+
+            temp = get_all_disease_samples_and_rest(X, y, novel_disease_label, raw_data)
+            all_samples_features, all_samples_labels, rest_features, rest_labels = temp
+            X_test_with_disease, y_test_with_disease = add_disease_to_test_data(X_test, y_test, rest_features,
+                                                                                rest_labels)
+            for n_examples in N_GIVEN_EXAMPLES:
+                temp = add_n_samples_to_train_data(X_train, y_train, all_samples_features, all_samples_labels,
+                                                   n_examples)
+                X_train_with_disease_samples, y_train_with_disease_samples, n_samples_features = temp
+
+                cls_without_generator = Classifier(n_classes=N_CLASSES, n_epochs=1)
+                cls_without_generator.fit(X_train_with_disease_samples, new_onehot_encode(y_train_with_disease_samples))
+                results_without_generator, fpr_without, tpr_without = evaluate_cls(cls_without_generator,
+                                                                                   X_test_with_disease,
+                                                                                   y_test_with_disease)
+                plt_name = '%d examples without generated samples' % n_examples
+                create_cls_roc_plot(fpr_without, tpr_without, results_without_generator, plt_name, disease_path)
+
+                generated_features = LowShotGenerator.get_generated_features(disease_base_cls,
+                                                                             novel_disease_label,
+                                                                             n_samples_features,
+                                                                             NUMBER_OF_CLUSTERS,
+                                                                             λ,
+                                                                             NUMBER_OF_TOTAL_GENRATED_EXAMPLES)
+
+                temp = add_generated_data_to_train_data(X_train_with_disease_samples, y_train_with_disease_samples,
+                                                        generated_features, novel_disease_label)
+                X_train_with_generated_data, y_train_with_generated_data = temp
+
+                cls_with_generator = Classifier(n_classes=N_CLASSES, n_epochs=1)
+                cls_with_generator.fit(X_train_with_generated_data, new_onehot_encode(y_train_with_generated_data))
+
+                results_with_generator, fpr_with, tpr_with = evaluate_cls(cls_with_generator, X_test_with_disease,
+                                                                          y_test_with_disease)
+                create_cls_roc_plot(fpr_with, tpr_with, results_with_generator,
+                                    '%d examples with generated samples' % n_examples, disease_path)
+
+                low_shot_learning_results[disease][n_examples] = {'with': results_with_generator[disease],
+                                                                  'without': results_without_generator[disease]}
+
+        return low_shot_learning_results
 
 if __name__ == "__main__":
     cls = MnistClassifier()
