e4e4eb1b7907e32857471ad0e011293196215f43
uridabomb <uriavron@gmail.com>
2018-08-31 02:31:22 +0300

    adds collection of the complete mnist

--- project/code/collect_mnist.sh
+++ project/code/collect_mnist.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 python3 collect.py -d mnist_0 -c 30;
 python3 collect.py -d mnist_1 -c 30;
 python3 collect.py -d mnist_2 -c 30;
@@ -8,5 +9,6 @@ python3 collect.py -d mnist_6 -c 30;
 python3 collect.py -d mnist_7 -c 30;
 python3 collect.py -d mnist_8 -c 30;
 python3 collect.py -d mnist_9 -c 30;
+python3 collect.py -d mnist -c 30;
 mv ../data/pickles/write/mnist* ../data/pickles/read/
 mv ../data/pickles/write/quadruplets/mnist* ../data/pickles/read/quadruplets/
