a6978db893f9f7c540bcffd90d5b8e8afaddbd68
uridabomb <uriavron@gmail.com>
2018-08-31 20:50:50 +0300

    adds plotting

--- project/code/benchmark.py
+++ project/code/benchmark.py
@@ -5,6 +5,8 @@ from cifar_data import *
 from itertools import product
 
 import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
 
 Classifiers = {'mnist': MnistClassifier}
 
@@ -13,35 +15,50 @@ data_obj_getters = {'mnist': MnistData,
 
 
 def main(dataset, category, n_clusters, generator_epochs, classifier_epochs, n_new, to_cross_validate, smart_category,
-         smart_centroids):
-    dataset_key = dataset.replace('raw_', '')
-    dataset_name = '{0}_{1}'.format(dataset, category)
+         smart_centroids, plot):
+    categories = range(data_obj_getters[dataset]().get_num_classes()) if category == 'all' else [category]
+    results = {}
 
-    def _benchmark(_hs, _λ):
-        return LowShotGenerator.benchmark_single(Classifiers[dataset_key],
-                                                 data_obj_getters[dataset_key],
-                                                 dataset_name,
-                                                 n_clusters=n_clusters,
-                                                 λ=_λ,
-                                                 hidden_size=_hs,
-                                                 n_new=n_new,
-                                                 epochs=generator_epochs,
-                                                 classifier_epochs=classifier_epochs,
-                                                 smart_category=smart_category,
-                                                 smart_centroids=smart_centroids)
+    for category in categories:
+        dataset_key = dataset.replace('raw_', '')
+        dataset_name = '{0}_{1}'.format(dataset, category)
 
-    if to_cross_validate:
-        raise NotImplementedError('Still does not work.')
-        hidden_sizes = (64, 128, 256, 512)
-        lambdas = (.1, .25, .5, .75, .9, .95)
+        def _benchmark(_hs, _λ):
+            return LowShotGenerator.benchmark_single(Classifiers[dataset_key],
+                                                     data_obj_getters[dataset_key],
+                                                     dataset_name,
+                                                     n_clusters=n_clusters,
+                                                     λ=_λ,
+                                                     hidden_size=_hs,
+                                                     n_new=n_new,
+                                                     epochs=generator_epochs,
+                                                     classifier_epochs=classifier_epochs,
+                                                     smart_category=smart_category,
+                                                     smart_centroids=smart_centroids)
 
-        losses, accs, n_uniques = {}, {}, {}
+        if to_cross_validate:
+            raise NotImplementedError('Still does not work.')
+            hidden_sizes = (64, 128, 256, 512)
+            lambdas = (.1, .25, .5, .75, .9, .95)
 
-        for hs, λ in product(hidden_sizes, lambdas):
-            losses[hs, λ], accs[hs, λ], n_uniques[hs, λ] = _benchmark(hs, λ)
+            losses, accs, n_uniques = {}, {}, {}
 
-    else:
-        _benchmark(256, .95)
+            for hs, λ in product(hidden_sizes, lambdas):
+                losses[hs, λ], accs[hs, λ], n_uniques[hs, λ] = _benchmark(hs, λ)
+
+        else:
+            loss, acc, n_unique = _benchmark(256, .95)
+            results[category] = {'loss': loss, 'acc': acc, 'n_unique': n_unique}
+
+        if plot:
+            results['avg'] = {'loss': np.average([v['loss'] for k, v in results.items()]),
+                              'acc': np.average([v['acc'] for k, v in results.items()]),
+                              'n_unique': np.average([v['n_unique'] for k, v in results.items()])}
+
+            df = pd.DataFrame.from_dict(results)
+            df.plot()
+            plt.tight_layout()
+            plt.savefig('./benchmark.png')
 
 
 if __name__ == "__main__":
@@ -54,12 +71,13 @@ if __name__ == "__main__":
     parser.add_argument('-ce', '--classifier_epochs', help='number of epcohs to train the classifier with', type=int,
                         default=1)
     parser.add_argument('-n', '--n_new', help='num of new examples to create and evaluate', type=int, default=100)
-    parser.add_argument('-cv', '--cross_validate', help='where to do a cross validation', action='store_true')
+    parser.add_argument('-cv', '--cross_validate', help='whether to do a cross validation', action='store_true')
     parser.add_argument('-sca', '--smart_category', help='wether to generate with samrt category', action='store_true')
     parser.add_argument('-sce', '--smart_centroids', help='wether to generate with samrt centroids',
                         type=str, default='none')
+    parser.add_argument('-p', '--plot', help='whether to plot the results', action='store_true')
 
     args = parser.parse_args()
 
     main(args.dataset, args.category, args.n_clusters, args.generator_epochs, args.classifier_epochs, args.n_new,
-         args.cross_validate, args.smart_category, args.smart_centroids)
+         args.cross_validate, args.smart_category, args.smart_centroids, args.plot)
