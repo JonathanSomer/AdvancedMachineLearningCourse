cce18745b6b41755f3d3140a641b7fa9230b7d1a
= <matan.harel.mh@gmail.com>
2018-09-02 00:48:45 +0300

    add fix_class-imbalance flag to pipeline_cv

--- project/code/pipeline_cv.py
+++ project/code/pipeline_cv.py
@@ -12,14 +12,14 @@ hidden_sizes = (64, 128, 256, 512)
 
 
 def run_cv(dataset_type, cls_type, use_data_subset=False, use_features=True, use_class_weights=True,
-                 generator_epochs=2, classifier_epochs=12, n_clusters=30):
+                 generator_epochs=2, classifier_epochs=12, n_clusters=30, fix_class_imbalance=False):
 
     base_dir = config.local_results_dir
     for hidden_size in hidden_sizes:
         for λ in λs:
             _logger.info('run for  hidden_size=%d and λ=%f' % (hidden_size, λ))
             pipeline.Pipeline(dataset_type, cls_type, use_data_subset, use_features, use_class_weights,
-                 generator_epochs, classifier_epochs, n_clusters, hidden_size, λ)
+                 generator_epochs, classifier_epochs, n_clusters, hidden_size, λ, fix_class_imbalance=fix_class_imbalance)
 
             new_dir_path = os.path.join(base_dir, '%d_%f'%(hidden_size, λ))
             os.makedirs(new_dir_path)
@@ -40,6 +40,7 @@ if __name__ == "__main__":
                         action='store_true')
     parser.add_argument('-c', '--n_clusters', help='number of clusters to use', type=int, default=30)
     parser.add_argument('-n', '--n_total', help='number of examples + generated', type=int, default=pipeline.N_GIVEN_EXAMPLES[-1])
+    parser.add_argument('-fc', '--fix_class_imbalance', help='uniformally sample from classes', action='store_true')
 
     args = parser.parse_args()
 
@@ -51,6 +52,7 @@ if __name__ == "__main__":
                 use_class_weights=not args.without_weights,
                 classifier_epochs=args.classifier_epochs,
                 generator_epochs=args.generator_epochs,
-                n_clusters=args.n_clusters)
+                n_clusters=args.n_clusters,
+                fix_class_imbalance=args.fix_class_imbalance)
     else:
         _logger.error('unknown dataset %s' % args.dataset)
